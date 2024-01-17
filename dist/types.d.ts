2c78df6b4bde069045be096dcbc77cc27a8cdb08
Federico <federico.rabbiosi@gmail.com>
2024-01-17 17:06:56 +0100

    1.0.7

---
+++ dist/types.d.ts
@@ -0,0 +1,17 @@
+import React, { ReactNode, CSSProperties } from 'react';
+
+interface IQRCodeReaderProps {
+    onResult: (code: string) => void;
+    loadingComponent?: ReactNode;
+    deviceModelName?: string;
+    style?: CSSProperties;
+}
+
+/**
+ * Read QRCode using decodeFromConstraints
+ * @param props
+ * @returns
+ */
+declare const QRCodeReader: (props: IQRCodeReaderProps) => React.JSX.Element;
+
+export { QRCodeReader as default };
